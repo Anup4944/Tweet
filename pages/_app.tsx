18f40ec7cdb754abd9772c3f9db2d3c28440a82d
Anup <anuppoudel60@gmail.com>
2023-04-09 21:37:25 +1000

    modal component created

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import Layout from "@/components/Layout";
-import Modal from "@/components/Modal";
+// import Modal from "@/components/Modal";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Modal />
+      {/* <Modal actionLabel="Submit" isOpen title="Test" /> */}
       <Layout>
         <Component {...pageProps} />
       </Layout>
