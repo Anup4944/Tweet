ef5b7325d12c21b6e746fd92eca0acbf66817ff6
Anup <anuppoudel60@gmail.com>
2023-04-23 22:29:55 +1000

    all post fetched

--- components/posts/PostItem.tsx
+++ components/posts/PostItem.tsx
@@ -3,6 +3,8 @@ import useLoginModel from "@/hooks/useLoginModel";
 import { formatDistanceToNowStrict } from "date-fns";
 import { useRouter } from "next/router";
 import React, { useCallback, useMemo } from "react";
+import Avatar from "../Avatar";
+import { AiOutlineHeart, AiOutlineMessage } from "react-icons/ai";
 
 interface PostItemProps {
   data: Record<string, any>;
@@ -49,7 +51,40 @@ const PostItem: React.FC<PostItemProps> = ({ data, userId }) => {
       className="border-b-[1px] border-neutral-800
     p-5 cursor-pointer hover:bg-neutral-900 transition"
     >
-      <div className="flex flex-row gap-3"></div>
+      <div className="flex flex-row gap-3">
+        <Avatar userId={data.user.id} />
+        <div>
+          <div className="flex flex-row items-center gap-2">
+            <p
+              className="text-white font-semibold cursor-pointer hover:underline"
+              onClick={goToUser}
+            >
+              {data.user.name}
+            </p>
+            <span
+              className="text-neutral-500 cursor-pointer hover:underline hidden md:block"
+              onClick={goToUser}
+            >
+              @{data.user.username}
+            </span>
+            <span className="text-neutral-500 text-sm">{createdAt}</span>
+          </div>
+          <div className="text-white mt-1">{data.body}</div>
+          <div className="flex flex-row items-center mt-3 gap-10">
+            <div className="flex flex-row items-center text-neutral-500 cursor-pointer transition hover:text-sky-500">
+              <AiOutlineMessage size={20} />
+              <p className="ml-2">{data.comments?.length || 0}</p>
+            </div>
+            <div
+              className="flex flex-row items-center text-neutral-500 cursor-pointer transition hover:text-red-500"
+              onClick={onLike}
+            >
+              <AiOutlineHeart size={20} onClick={onLike} />
+              <p className="ml-2">{data.comments?.length || 0}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
