da673a1aefc897819cc7e9607f2b259a9dc75a01
Anup <anuppoudel60@gmail.com>
2023-06-19 21:35:18 +1000

    app.tsx fixed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
-import Layout from "@/components/Layout";
-import LoginModal from "@/components/modals/LoginModal";
-import RegisterModel from "@/components/modals/RegisterModel";
-// import Modal from "@/components/Modal";
-import "@/styles/globals.css";
+import Layout from "../components/Layout";
+import LoginModal from "../components/modals/LoginModal";
+import RegisterModel from "../components/modals/RegisterModel";
+// import Modal from "../components/Modal";
+import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
 import { SessionProvider } from "next-auth/react";
-import EditModal from "@/components/modals/EditModal";
+import EditModal from "../components/modals/EditModal";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
