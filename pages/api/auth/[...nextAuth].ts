4458bfd15717176dc3c565198b26b028563c824c
Anup <anuppoudel60@gmail.com>
2023-08-11 22:46:43 +1000

    changes on path

--- pages/api/auth/[...nextAuth].ts
+++ pages/api/auth/[...nextAuth].ts
@@ -2,7 +2,7 @@ import bcrypt from "bcrypt";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "../../../libs/prismadb";
+import prisma from "@/libs/prismadb";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
