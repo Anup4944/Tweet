5323edb46ae48a2e07b201b2ad00e9f0363f9e71
Anup <anuppoudel60@gmail.com>
2023-08-11 23:13:29 +1000

    nextauth modified

--- pages/api/auth/[...nextAuth].ts
+++ pages/api/auth/[...nextAuth].ts
@@ -1,10 +1,10 @@
 import bcrypt from "bcrypt";
-import NextAuth, { NextAuthOptions } from "next-auth";
+import NextAuth, { AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/libs/prismadb";
 
-export const authOptions: NextAuthOptions = {
+export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
